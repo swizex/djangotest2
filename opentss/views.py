43db4a60418e90c14a19ba37af6fe5c590e4ff47
Teddy Morduhovich <swizex@gmail.com>
2017-12-25 08:09:59 +0200

    initial commit

--- opentss/views.py
+++ opentss/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
+from django.template import Template, Context
+from django.template.loader import render_to_string
+import datetime
 
 
 # Create your views here.
@@ -9,7 +12,18 @@ def home(request):
 
 
 def index(request):
-    return TemplateResponse(request,'index.html')
+    now = datetime.datetime.now()
+    testvariable = "this is a variable from python!"
+
+    #fp = open('index.html')
+    #t = Template(fp.read())
+    #fp.close()
+
+    #html = t.render(Context({now}))
+    html = render_to_string('index.html', {'now': now,'testv':testvariable})
+
+    #return TemplateResponse(request,'index.html')
+    return HttpResponse(html)
 
 
 def login(request):
@@ -17,4 +31,4 @@ def login(request):
 
 
 def register(request):
-    return  TemplateResponse(request, 'register.html')
+    return TemplateResponse(request, 'register.html')
